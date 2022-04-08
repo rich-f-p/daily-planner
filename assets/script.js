9f5b684768b5fbe35d94b7edc55c3234f5ac238c
rich p <richardferry3@gmail.com>
2022-04-07 22:10:06 -0400

    add function to getitems from local storage and convert data string to integer

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+// current day is displayed at the top of the calendar
 function displayTime(){
-    time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+    time = moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
     $('#currentDay').text(time);
 }
+// updates clock every second
 function clock(){
     displayTime();
     setInterval(function(){
@@ -12,16 +12,13 @@ function clock(){
 
     }, 1000)
 }
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
+// timeblock is color coded to indicate whether it is in the past, present, or future
 function color(){
     $('textarea').each(function(){
-        var blockTime= $(this).attr('data');
-        var timeHour= moment().format('H'); 
+        var blockTime = parseInt($(this).attr('data'));
+        var timeHour =  moment().format('H');  
+        console.log(blockTime)
+        console.log(timeHour)
         if (timeHour>blockTime){
             $(this).removeClass('present')
             $(this).removeClass('future')
@@ -37,13 +34,10 @@ function color(){
         }
     })
 }
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
+// enter an event
+// event is saved in local storage through click on button
 $('.saveBtn').on('click',function(){
-        var timeOfEvent = $(this).siblings('.description').attr("data");
+        var timeOfEvent = $(this).siblings('.description').attr('data');
         console.log(timeOfEvent)
         var perform = $(this).siblings('.description').val();
         console.log(perform)
@@ -52,10 +46,13 @@ $('.saveBtn').on('click',function(){
         }
         save();
 })
-
-
 // WHEN I refresh the page
 // THEN the saved events persist
+function refresh(){
+    $('*[data]').each(function () {
+        $(this).val(localStorage.getItem($(this).attr('data')));
+      });
+}
 clock();
 color();
-/* how to generate time blocks in js */
+refresh();
